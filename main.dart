5ae4c485e1288880bbf62821d4de9a8dfe988643
murat143-cpu <murat143@gmail.com>
2026-08-17 19:17:38 +0300

    Delete main.dart

--- main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-void main()=>runApp(const App());
-class App extends StatelessWidget{const App({super.key});@override Widget build(BuildContext c)=>MaterialApp(debugShowCheckedModeBanner:false,title:'Varant Direction AI',theme:ThemeData(brightness:Brightness.dark,useMaterial3:true,colorSchemeSeed:Colors.blue),home:const HomeScreen());}
